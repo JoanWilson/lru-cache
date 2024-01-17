e37a54fb74399a7a843d84b98db215b2ba4a68c7
Joan Wilson <joanwmoliveira@gmail.com>
2024-01-17 11:39:55 -0300

    changed keys and values to string and any value

--- main.go
+++ main.go
@@ -6,30 +6,30 @@ import (
 )
 
 type Node struct {
-	Data    int
+	Data    any
 	Pointer *list.Element
 }
 
 type LRUCache struct {
 	Queue    *list.List
-	Items    map[int]*Node
+	Items    map[string]*Node
 	Capacity int
 }
 
 func NewLRUCache(capacity int) LRUCache {
 	return LRUCache{
 		Queue: list.New(), 
-		Items: make(map[int]*Node),
+		Items: make(map[string]*Node),
 		Capacity: capacity,
 	}
 }
 
-func (l *LRUCache) Put(key int, value int) {
+func (l *LRUCache) Set(key string, value any) {
 	if item, ok := l.Items[key]; !ok {
 		if l.Capacity == len(l.Items) {
 			back := l.Queue.Back()
 			l.Queue.Remove(back)
-			delete(l.Items, back.Value.(int))
+			delete(l.Items, back.Value.(string))
 		}
 		l.Items[key] = &Node{Data: value, Pointer: l.Queue.PushFront(key)}
 	} else {
@@ -39,7 +39,7 @@ func (l *LRUCache) Put(key int, value int) {
 	}
 }
 
-func (l *LRUCache) Get(key int) int {
+func (l *LRUCache) Get(key string) any {
 	if item, ok := l.Items[key]; ok {
 		l.Queue.MoveToFront(item.Pointer)
 		return item.Data
@@ -51,10 +51,13 @@ func (l *LRUCache) Get(key int) int {
 func main()  {
 	fmt.Println("==> LRUCache Test Case <==")
 	obj := NewLRUCache(2)
-	obj.Put(2, 1)
-	obj.Put(1, 1)
-	obj.Put(4, 1)
-	obj.Put(5, 7)
-	fmt.Println(obj.Get(4))
-	fmt.Println(obj.Get(5))
+	obj.Set("joan", 26)
+	obj.Set("patricia", 28)
+	obj.Set("wilson", 25)
+	obj.Set("fulano", 20)
+	
+	fmt.Println(obj.Get("joan"))
+	fmt.Println(obj.Get("patricia"))
+	fmt.Println(obj.Get("wilson"))
+	fmt.Println(obj.Get("fulano"))
 }
